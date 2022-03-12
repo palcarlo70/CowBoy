4e05fd871ae382f70a7c6b1a6c7bfdbbc064e9cc
Carlo <PalCarlo@hotmail.com>
2022-03-12 21:30:55 +0100

    ff

--- CowBoyWeb/Controllers/HomeController.cs
+++ CowBoyWeb/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AvalonWeb.Models;
+using CowBoyWeb.Models;
 using Newtonsoft.Json;
 
 namespace CowBoyWeb.Controllers
