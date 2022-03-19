3daef59ae943d4920eaed181170d39c56a123be4
Carlo <PalCarlo@hotmail.com>
2022-03-19 22:54:00 +0100

    hghfh

--- CowBoyWeb/Controllers/HomeController.cs
+++ CowBoyWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,18 +14,15 @@ namespace CowBoyWeb.Controllers
         readonly JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
         public ActionResult Index()
         {
-
+            return View();
+        }
+        public ActionResult DettaglioBovini()
+        {
             return View();
         }
 
 
-        //public ContentResult GetBovini()
-        //{
-        //    var clCom = new ClassiComuni();
-        //    var conn = new CowBoy.ComponentsNew.BoviniCom();
-        //    var lst = conn.GetBovini(null,null,null,null,null,null,null, clCom.ConnectCbUniversal);
-        //    return Content(JsonConvert.SerializeObject(lst, _jsonSetting), "application/json");
-        //}
+
 
         public ContentResult GetBovini(int? idAnagrafica, string sesso, int? manze, int? inLattazione, int? inAsciutta, string ricercaLibera, int? inAzienda)
         {
@@ -42,6 +40,25 @@ namespace CowBoyWeb.Controllers
             var clCom = new ClassiComuni();
             var conn = new CowBoy.ComponentsNew.BoviniCom();
             var lst = conn.GetProssimiSaltiAsciutteParti(clCom.ConnectCbUniversal);
+            return Content(JsonConvert.SerializeObject(lst, _jsonSetting), "application/json");
+        }
+
+        public ContentResult UploadImage(HttpPostedFileBase file)
+        {
+            string[] lst = new string[2];
+            
+            try
+            {
+                string fileName = Path.GetFileName(file.FileName);
+                string dest = Directory.CreateDirectory(Server.MapPath("~/img/FotoBov")).FullName;
+                string filePath = Path.Combine(dest, fileName);
+                file.SaveAs(filePath);
+                lst[0] = fileName;
+            }
+            catch (Exception ex)
+            {
+                lst[1] = ex.Message;
+            }
             return Content(JsonConvert.SerializeObject(lst, _jsonSetting), "application/json");
         }
 
---
+++ CowBoyWeb/Scripts/personal/dettaglioBovino.js
@@ -0,0 +1,73 @@
+﻿$(function () {
+    //$("#txtDataNascita").datepicker({
+    //    format: "dd/mm/yyyy",
+    //    autoclose: true
+    //});
+    //$('.datepicker').datepicker({
+    //    format: 'mm/dd/yyyy',
+    //    autoclose: true
+    //});
+    //$('.datepicker').on('changeDate', function (event) {
+    //    $(this).datepicker('hide');
+    //});
+    $('#txtDataNascita').datepicker({}).on('change', function () { $('.datepicker').hide(); });
+    $('#txtDataUscita').datepicker({}).on('change', function () { $('.datepicker').hide(); });
+});
+
+
+
+
+
+
+$(document).on("change", "#inputNewImage", function () {
+    if (this.files.length > 0) {
+        $("#spanNewImage").text(this.files[0].name);
+        $("#imgUploadUteNew").attr("src", URL.createObjectURL(this.files[0]))
+    }
+});
+
+function BovNewImageUpload() {
+    if ($('#inputNewImage').val() == "") {
+        alert("Scegli un file");
+        return false
+    }
+    var file = $("#inputNewImage")[0].files[0];
+    var formData = new FormData();
+    formData.append("file", file);
+
+    $.ajax({
+        url: "../Home/UploadImage",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            if (response !== null) {
+                var path = response[0];
+                if (path !== "" && path !== null) {
+                    $("#imgBov").attr("src", "../img/FotoUte/" + path);
+                    $("#txtBovPathImg").val(path);
+                    $("#modalBovNewImagePopUp").modal('hide');
+                }
+            }
+        },
+        failure: function (response) {
+            alert("Errore nel caricamento");
+        }
+    });
+
+}
+
+function ChangeNewImage() {
+    var path = "../img/FotoBov/" + $("#txtBovPathImg").val();
+    $("#imgBov").attr("src", path);
+}
+
+
+
+function ApriPopUpNewImage() {
+    $("#modalBovNewImagePopUp").modal('show');
+    $('#inputNewImage').val('');
+    $('#spanNewImage').text("File non selezionato");
+    $("#imgUploadUteNew").attr("src", "");
+}
